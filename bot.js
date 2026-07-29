0354b28b032d913cf5f2119fa17639a2d898c6a5
bandera33333 <nemobandera31@gmail.com>
2026-07-29 15:26:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,24 @@
 const TelegramBot = require('node-telegram-bot-api');
+const http = require('http');
 
 const token = process.env.BOT_TOKEN;
 
 const bot = new TelegramBot(token, { polling: true });
 
+// Petit serveur pour Render
+const PORT = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('Bot Telegram actif');
+}).listen(PORT, () => {
+  console.log(`Serveur lancé sur le port ${PORT}`);
+});
+
 bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat.id, 
-    "🎬 Bienvenue sur le vote des séries !\n\nChoisis ta série préférée."
+  bot.sendMessage(
+    msg.chat.id,
+    "🎬 Bienvenue sur le vote des séries !\n\nLe bot fonctionne ✅"
   );
 });
 
