fae7da0c47feea9578df0691e34be5b4ec6c5573
bandera33333 <nemobandera31@gmail.com>
2026-07-29 16:13:38 +0200

    Update series.js

--- series.js
+++ series.js
@@ -1,9 +1,24 @@
 const series = [
-  "12 Monkeys 2015",
-  "13 Reasons Why 2017",
-  "1923 2022",
-  "Breaking Bad 2008",
-  "Game Of Thrones 2011"
+  "Alien: Earth 2025",
+  "All American 2018",
+  "All Her Fault 2025",
+  "All Of Us Are Dead 2022",
+  "Allegiance 2024",
+  "Almost Paradise 2020",
+  "Alta Mar 2019",
+  "Alter Ego 2026",
+  "Altered Carbon 2018",
+  "American Dad! 2005",
+  "American Gods 2017",
+  "American Horror Stories 2021",
+  "American Horror Story 2011",
+  "American Housewife 2016",
+  "American Rust 2021",
+  "American Wives 2007",
+  "Amicalement Vôtre 1971",
+  "Anaon 2025",
+  "Ancestral 2024",
+  "Andromeda 2000"
 ];
 
 module.exports = series;
